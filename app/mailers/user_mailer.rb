9d941e72ad6c1ef43dd6297d9307487917f779b7
DanyArg <daniela.feversani@gmail.com>
2012-03-21 08:20:05 -0300

    Update app/mailers/user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default :from => "daniela.feversani@n3tsur.com"
+  default :from => "miCta@n3tsur.com"
 
   def registration_confirmation(user)
 	@user = user
   	@url  = "http://www.gmail.com"
-  	mail(:to=>"daniela.feversani@gmail.com", :subject=>"Bienvenido")
+  	mail(:to=>"miCta@gmail.com", :subject=>"Bienvenido")
 #  mail(:to =>  "user.email", :subject => "Registered")
   end
 
